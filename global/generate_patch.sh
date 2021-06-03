d0522d10ef9657277af75258e50ea464fdeeade8
Ben Alman <cowboy@rj3.net>
2021-06-03 15:57:59 -0400

    Update patch generator to be more usable

--- global/generate_patch.sh
+++ global/generate_patch.sh
@@ -1,9 +1,39 @@
 #!/usr/bin/env bash
 
-if [[ -z "$1" ]]; then
-  echo "Usage: $0 commit_sha"
+set -o errexit
+set -o pipefail
+set -o nounset
+# set -o xtrace
+
+if [[ -z "${1:-}" ]]; then
+  echo "Usage: $(basename "$0") commit [patch_name]"
   exit 1
 fi
 
-# Generate patch from a specific commit, excluding lock files
-git diff $1^..$1 ':(exclude)*lock*'
+commit=$1
+function get_diff() {
+  git diff $commit^..$commit ':(exclude)*lock*'
+}
+
+if [[ -z "${2:-}" ]]; then
+  echo "Diff for commit $commit"
+  get_diff
+  exit
+fi
+
+patch_name=$2
+
+parent_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." &> /dev/null && pwd)"
+
+current_branch=$(git branch --show-current)
+patch_dir_abs="$parent_dir/$current_branch"
+patch_file="$current_branch/$patch_name.patch"
+patch_file_abs="$parent_dir/$patch_file"
+
+if [[ ! -d "$patch_dir_abs" ]]; then
+  echo "Creating $patch_dir_abs"
+  mkdir "$patch_dir_abs"
+fi
+
+echo "Generating patch file $patch_file from commit $commit"
+get_diff > "$patch_file_abs"
