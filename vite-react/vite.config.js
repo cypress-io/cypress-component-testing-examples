0e14ce0c4f3890004b9db32e822dca56839761ec
Kevin Old <kevin@cypress.io>
2021-09-30 16:50:51 -0500

    configure vite-plugin-istanbul

--- vite-react/vite.config.js
+++ vite-react/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import istanbul from 'vite-plugin-istanbul';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [
+    reactRefresh(),
+    istanbul({
+      include: 'src/*',
+      exclude: ['node_modules'],
+      extension: [ '.js', '.jsx', '.ts' ],
+      requireEnv: true,
+      cypress: true
+    }),
+  ]
 })
