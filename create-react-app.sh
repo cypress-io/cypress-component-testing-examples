8818b40cc62dd14e1904a1ea281716a31581f833
Kevin Old <kevin@cypress.io>
2021-10-14 11:42:37 -0500

    Remove scripts and directories and point to new location in main/scripts

--- create-react-app.sh
+++ create-react-app.sh
@@ -1,26 +1,2 @@
-#!/usr/bin/env bash
-
-set -o errexit
-set -o pipefail
-set -o nounset
-# set -o xtrace
-
-script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
-source "$script_dir/global/functions.sh"
-
-title='Create React App'
-
-create_branch
-
-step
-cmd npx create-react-app $dir
-cmd cd $dir
-commit_all "Run: ${commands[-2]}"
-
-step
-cmd yarn add -D cypress @cypress/react @cypress/webpack-dev-server eslint-plugin-cypress
-apply_patch global/create-react-app.patch
-apply_patch
-commit_all "Add Cypress with example component test"
-
-finalize
+# This script has been moved to the main branch
+# https://github.com/cypress-io/cypress-component-testing-examples/tree/main/scripts/create-react-app.sh
--- global/pr_header.md
+++
@@ -1,9 +0,0 @@
-# Component Testing Example: ${TITLE}
-
-This is an example **${TITLE}** project with Cypress component testing.
-
-- `yarn` to install the dependencies
-- `npx cypress open-ct` to run interactively
-- `npx cypress run-ct` to run all tests headlessly
-
-The following was done to create this example:
