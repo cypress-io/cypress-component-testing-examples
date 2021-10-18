618a57a86d76c7b6470d9e24d03a5930224e9ec1
Paul Jaffre <jaffrepaul@gmail.com>
2021-10-18 11:06:59 -0400

    install & configure snapshot plugin (vite-react)

--- vite-react/cypress/plugins/index.js
+++ vite-react/cypress/plugins/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const { startDevServer } = require('@cypress/vite-dev-server')
 const codeCoverageTask = require('@cypress/code-coverage/task')
 const istanbul = require('vite-plugin-istanbul')
+const { initPlugin } = require('cypress-plugin-snapshots/plugin');
 
 module.exports = (on, config) => {
   on('dev-server:start', (options) => {
@@ -15,6 +16,7 @@ module.exports = (on, config) => {
   })
 
   codeCoverageTask(on, config);
-
+  initPlugin(on, config);
+  
   return config
 }
