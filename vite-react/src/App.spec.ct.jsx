f0f6a6b59a2a162fe5eb00bd13d033bf7c6dd356
Paul Jaffre <jaffrepaul@gmail.com>
2021-10-18 11:21:31 -0400

    initial snapshot test (vite-react)

--- vite-react/src/App.spec.ct.jsx
+++ vite-react/src/App.spec.ct.jsx
@@ -4,5 +4,8 @@ import App from './App'
 
 it('renders learn react link', () => {
   mount(<App />)
-  cy.get('a').contains('Learn React')
+  cy.get('a').contains('Learn React'); // core Cypress method
+  cy.get('a').toMatchSnapshot(); // serialized element snapshot
+  cy.get('a').toMatchImageSnapshot(); // targeted element image snapshot
+  cy.get('.App').toMatchImageSnapshot(); // fullscreen image snapshot
 })
