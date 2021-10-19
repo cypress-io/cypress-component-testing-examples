f7e0cf0fbf3f8c681d6874bf74a35126b1c82cb2
Kevin Old <kevin@cypress.io>
2021-10-19 17:11:44 -0500

    configure CNA with cypress code coverage

--- create-next-app/cypress/plugins/index.js
+++ create-next-app/cypress/plugins/index.js
@@ -1,6 +1,8 @@
 const injectDevServer = require('@cypress/react/plugins/next')
+const codeCoverageTask = require('@cypress/code-coverage/task')
 
 module.exports = (on, config) => {
   injectDevServer(on, config)
+  codeCoverageTask(on, config)
   return config
 }
