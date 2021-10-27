afe31480d5bf003f86567a7d9eeb1d905a9c7ac1
Kevin Old <kevin@cypress.io>
2021-10-27 17:04:01 -0500

    wip - update App to interact with application to allow for code coverage

--- vue-cli-3/src/App.spec.ct.js
+++ vue-cli-3/src/App.spec.ct.js
@@ -5,4 +5,7 @@ it('shows the App', () => {
   mount(App)
   cy.get('[alt="Vue logo"]')
   cy.get('h1').contains('Welcome to Your Vue.js App')
+  cy.get('button').click()
+  cy.get('[data-test=count]').should("have.text", 1)
+  cy.get('button').click()
 })
--- vue-cli-3/src/App.vue
+++ vue-cli-3/src/App.vue
@@ -1,15 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p data-test="count">{{count}}</p>
+    <button @click="increment">Increment</button>
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+function incrementFunction(count) {
+  return count + 1;
+}
+
 export default {
   name: 'App',
+  data (){
+    return {
+      count: 0
+    }
+  },
   components: {
     HelloWorld
+  },
+  methods: {
+    increment() {
+      console.log('THE COUNT', this.count)
+      this.count = incrementFunction(this.count)
+    }
   }
 }
 </script>
