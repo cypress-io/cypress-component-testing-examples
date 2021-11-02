91736d280f9c47656946bbcdaea7c8619bcdc912
Kevin Old <kevin@cypress.io>
2021-11-02 12:17:26 -0500

    update jest exclude

--- vue-cli-3/jest.config.js
+++ vue-cli-3/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     collectCoverage: true,
-    collectCoverageFrom: ["src/**/*.{js,vue}", '!src/**/*/*.spec.ct.js'],
+    collectCoverageFrom: ["src/**/*.{js,vue}", '!src/**/*.spec.ct.js'],
     preset: "@vue/cli-plugin-unit-jest",
     transform: {
       "^.+\\.vue$": "vue-jest",
