3d307cbd7c2226b34926fac3fc59bd74e36a17c2
Kevin Old <kevin@cypress.io>
2021-11-04 16:07:51 -0500

    Configure Cypress Code Coverage plugin

--- vite-react/cypress/plugins/index.js
+++ vite-react/cypress/plugins/index.js
@@ -1,15 +1,20 @@
 const path = require('path')
 const { startDevServer } = require('@cypress/vite-dev-server')
+const codeCoverageTask = require('@cypress/code-coverage/task')
+const istanbul = require('vite-plugin-istanbul')
 
 module.exports = (on, config) => {
   on('dev-server:start', (options) => {
     return startDevServer({
       options,
       viteConfig: {
-        configFile: path.resolve(__dirname, '../../vite.config.js'),
+        clearScreen: false,
+        plugins: [istanbul({})],
       },
     })
   })
+
+  codeCoverageTask(on, config);
 
   return config
 }
