f85dda0b1b4c736740cac89914fe1a6bcd281ac0
Kevin Old <kevin@cypress.io>
2021-11-04 16:07:36 -0500

    Refactor app into separate components, adjust global styles

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,19 +1,15 @@
-import { useState } from 'react'
 import logo from './logo.svg'
+import { Counter } from './Counter'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
+          <Counter />
         </p>
         <p>
           Edit <code>App.jsx</code> and save to test HMR updates.
