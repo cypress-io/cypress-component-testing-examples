f1966214b239e74a06edfffb75589c9967aeae8b
Ben Alman <cowboy@rj3.net>
2021-11-22 15:40:17 -0500

    Add tests following the getting started guide

--- example-react/src/components/LoginForm.cy.js
+++ example-react/src/components/LoginForm.cy.js
@@ -1,107 +1,86 @@
-import { mount } from '@cypress/react';
-import { LoginForm } from './LoginForm';
+import { mount } from '@cypress/react'
+import { LoginForm } from './LoginForm'
 
 describe('LoginForm', () => {
-  const username = 'johndoe';
-  const password = 's3cret';
+  it('should mount the component', () => {
+    mount(<LoginForm />)
+  })
 
-  describe('when form is initialized', () => {
-    it('should render title with default text', () => {
-      mount(<LoginForm />);
-      cy.get('legend').should('have.text', 'Log In');
-    });
+  it('should have password input of type password', () => {
+    mount(<LoginForm />)
+    cy.contains('Password')
+      .find('input')
+      .should('have.attr', 'type', 'password')
+  })
 
-    it('should renders title with supplied text', () => {
-      const title = 'Please Authenticate';
-      mount(<LoginForm title={title} />);
-      cy.get('legend').should('have.text', title);
-    });
+  it('should render title with default text', () => {
+    mount(<LoginForm />)
+    cy.get('legend').should('have.text', 'Log In')
+  })
 
-    it('password input should be of type password', () => {
-      mount(<LoginForm />);
-      cy.get('label:contains("Password") input').should(
-        'have.attr',
-        'type',
-        'password'
-      );
-    });
-  });
+  it('should render title with specified text', () => {
+    const title = 'Please Authenticate'
+    mount(<LoginForm title={title} />)
+    cy.get('legend').should('have.text', title)
+  })
 
-  describe('when form is valid', () => {
-    let onLoginSpy;
+  describe('form tests', () => {
+    const username = 'testuser123'
+    const password = 's3cret'
 
     beforeEach(() => {
-      onLoginSpy = cy.spy().as('onLoginSpy');
-      mount(<LoginForm onLogin={onLoginSpy} />);
-      cy.get('label:contains("Username") input')
-        .as('usernameInput')
-        .type(username);
-      cy.get('label:contains("Password") input')
-        .as('passwordInput')
-        .type(password);
-      cy.get('button:contains("Login")').as('loginButton');
-    });
+      const onLoginSpy = cy.spy().as('onLoginSpy')
+      mount(<LoginForm onLogin={onLoginSpy} />)
+      cy.contains('Username').find('input').as('usernameInput')
+      cy.contains('Password').find('input').as('passwordInput')
+      cy.get('button').contains('Login').as('loginButton')
+    })
 
-    it('should call onLogin when login button is clicked', () => {
-      cy.get('@loginButton').click();
-      cy.get('@onLoginSpy').should('be.calledOnce');
-    });
-
-    it('should call onLogin when user clicks the enter key in one of the inputs', () => {
-      cy.get('@passwordInput').type('{enter}');
-      cy.get('@onLoginSpy').should('be.calledOnce');
-    });
-
-    it('onLogin should get username and password when form is submitted', () => {
-      cy.get('@loginButton').click();
+    it('should call onLogin with username and password when the Login button is clicked', () => {
+      cy.get('@usernameInput').type(username)
+      cy.get('@passwordInput').type(password)
+      cy.get('@loginButton').click()
       cy.get('@onLoginSpy').should('have.been.calledWith', {
         username,
         password,
-      });
-    });
-  });
+      })
+    })
 
-  describe('when form is invalid', () => {
-    let onLoginSpy;
-    const usernameErrorQuery = 'span.error:contains("Username is required")';
-    const passwordErrorQuery = 'span.error:contains("Password is required")';
+    it('should call onLogin with username and password when enter is pressed in an input', () => {
+      cy.get('@usernameInput').type(username)
+      cy.get('@passwordInput').type(password).type('{enter}')
+      cy.get('@onLoginSpy').should('have.been.calledWith', {
+        username,
+        password,
+      })
+    })
 
-    beforeEach(() => {
-      onLoginSpy = cy.spy().as('onLoginSpy');
-      mount(<LoginForm onLogin={onLoginSpy} />);
-      cy.get('label:contains("Username") input').as('usernameInput');
-      cy.get('label:contains("Password") input').as('passwordInput');
-      cy.get('button:contains("Login")').as('loginButton');
-    });
+    it('should show both validation errors if login is attempted without entering username or password', () => {
+      cy.get('@loginButton').click()
+      cy.contains('Username is required')
+      cy.contains('Password is required')
+      cy.get('@onLoginSpy').should('not.have.been.called')
+    })
 
-    it('should only show both validation errors after login button is clicked', () => {
-      cy.get('@loginButton').click();
-      cy.get('span.error').should('have.length', 2);
-    });
+    it('should only show password validation error if login is attempted without entering password', () => {
+      cy.get('@usernameInput').type(username)
+      cy.get('@loginButton').click()
+      cy.contains('Username is required').should('not.exist')
+      cy.contains('Password is required')
+      cy.get('@onLoginSpy').should('not.have.been.called')
+    })
 
-    it('should not show any validation errors before login button is clicked', () => {
-      cy.get('span.error').should('not.exist');
-      cy.get('@usernameInput').type('abc123').clear();
-      cy.get('span.error').should('not.exist');
-      cy.get('@passwordInput').type('abc123').clear();
-    });
+    it('should only show username validation error if login is attempted without entering username', () => {
+      cy.get('@passwordInput').type(password)
+      cy.get('@loginButton').click()
+      cy.contains('Username is required')
+      cy.contains('Password is required').should('not.exist')
+      cy.get('@onLoginSpy').should('not.have.been.called')
+    })
 
-    it('should show "Username is Required" error when form is submitted with username missing', () => {
-      cy.get('@loginButton').click();
-      cy.get('@passwordInput').type('abc123');
-      cy.get(usernameErrorQuery).should('exist');
-    });
-
-    it('should show "Password is Required" error when form is submitted with password missing', () => {
-      cy.get('@loginButton').click();
-      cy.get('@usernameInput').type('abc123');
-      cy.get(passwordErrorQuery).should('exist');
-    });
-
-    it('should show both errors when form is submitted with both fields missing', () => {
-      cy.get('@loginButton').click();
-      cy.get(usernameErrorQuery).should('exist');
-      cy.get(passwordErrorQuery).should('exist');
-    });
-  });
-});
+    it('should not show any validation errors before login is attempted', () => {
+      cy.contains('Username is required').should('not.exist')
+      cy.contains('Password is required').should('not.exist')
+    })
+  })
+})
