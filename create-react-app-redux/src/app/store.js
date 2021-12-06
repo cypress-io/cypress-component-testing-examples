e44c3dd70b69dfb7c167ce4ce9bc93ce81d899ff
Kevin Old <kevin@cypress.io>
2021-12-06 15:39:23 -0600

    Update app redux store to be more testable

--- create-react-app-redux/src/app/store.js
+++ create-react-app-redux/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 
-export const store = configureStore({
+export const getStore = () => configureStore({
   reducer: {
     counter: counterReducer,
   },
 });
+
+export const store = getStore();
