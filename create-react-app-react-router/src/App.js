820aac06b00cbb8098d5757a6d94618a945f8a60
Kevin Old <kevin@cypress.io>
2021-12-06 15:38:23 -0600

    Add react-router and update app to use it

--- create-react-app-react-router/src/App.js
+++ create-react-app-react-router/src/App.js
@@ -1,22 +1,28 @@
+import { Switch, Route, NavLink } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+
+const PageLink = (props) => (
+  <NavLink className="App-link" activeClassName="App-link-active" {...props} />
+);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="Page">
+          <Switch>
+            <Route exact path="/">
+              Home page content
+            </Route>
+            <Route path="/about">About page content</Route>
+          </Switch>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <PageLink exact to="/">
+          Home
+        </PageLink>
+        <PageLink to="/about">About</PageLink>
       </header>
     </div>
   );
