7197ef5e059d589df84a108376870749fea7c330
Kevin Old <kevin@cypress.io>
2021-12-07 16:48:27 -0600

    Add Cypress with example component and page tests

--- vue-cli-2/src/App.vue
+++ vue-cli-2/src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Counter />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Counter from './components/Counter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Counter
   }
 }
 </script>
