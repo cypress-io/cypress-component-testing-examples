6e9d060cb3b8bed2c7169a7c155dca5e2848ba38
Kevin Old <kevin@cypress.io>
2021-12-17 15:01:57 -0600

    Remove existing example files

--- create-nuxt-app/components/Logo.spec.ct.js
+++
@@ -1,7 +0,0 @@
-import { mount } from '@cypress/vue'
-import Logo from './Logo.vue'
-
-it('shows the logo', () => {
-  mount(Logo)
-  cy.get('svg').should('have.class', 'NuxtLogo')
-})
--- create-nuxt-app/pages/index.spec.ct.js
+++
@@ -1,8 +0,0 @@
-import { mount } from '@cypress/vue'
-import Index from './index.vue'
-
-it('shows the index', () => {
-  mount(Index)
-  cy.get('h1').contains('create-nuxt-app')
-  cy.get('a').should('have.length', 2)
-})
