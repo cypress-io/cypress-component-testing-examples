598a101681d5b5e820d14a32c63469d6af3110b0
Kevin Old <kevin@cypress.io>
2021-12-17 16:14:05 -0600

    Remove existing example files

--- vite-vue/src/App.spec.ct.js
+++
@@ -1,8 +0,0 @@
-import { mount } from '@cypress/vue'
-import App from './App.vue'
-
-it('shows the App', () => {
-  mount(App)
-  cy.get('[alt="Vue logo"]')
-  cy.get('h1').contains('Hello Vue 3 + Vite')
-})
