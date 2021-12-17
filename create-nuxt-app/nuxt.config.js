0a56a886bcc63428406aff5e7e545e9646a75b9e
Kevin Old <kevin@cypress.io>
2021-12-17 15:03:44 -0600

    Configure Cypress Code Coverage plugin and add additional component tests

--- create-nuxt-app/nuxt.config.js
+++ create-nuxt-app/nuxt.config.js
@@ -37,5 +37,11 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    extend(config) {
+      config.performance.hints = false
+    },
+    babel: {
+      babelrc: true
+    }
   }
 }
