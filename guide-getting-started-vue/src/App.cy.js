21029579979b68409fbaafc3f9be76efaae9f9c4
Ely Lucas <ely@meta-tek.net>
2022-01-07 11:48:54 -0700

    Add Cypress with example components and page tests

---
+++ guide-getting-started-vue/src/App.cy.js
@@ -0,0 +1,8 @@
+import { mount } from '@cypress/vue';
+import App from './App.vue';
+
+it('shows the App', () => {
+  mount(App);
+  cy.get('[alt="Vue logo"]');
+  cy.get('h1').contains('Welcome to Your Vue.js App');
+});
