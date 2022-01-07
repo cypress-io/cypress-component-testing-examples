cf4f5360a4a29f4591f2a52be23b59eebca856c8
Ely Lucas <ely@meta-tek.net>
2022-01-07 09:35:46 -0700

    updates for cra5 and cypress v10

--- scripts/guide-getting-started-react-complete.sh
+++ scripts/guide-getting-started-react-complete.sh
@@ -15,12 +15,12 @@ create_branch
 step
 cmd npx create-react-app guide-getting-started-react-complete
 cmd cd $dir
+cmd rm package-lock.json
 commit_all "Run: ${commands[-2]}"
 
 step
-cmd yarn add -D cypress @cypress/react @cypress/webpack-dev-server eslint-plugin-cypress
+cmd yarn add -D https://cdn.cypress.io/beta/npm/10.0.0/circle-10.0-release-0366d4fa8971e5e5189c6fd6450cc3c8d72dcfe1/cypress.tgz @cypress/react @cypress/webpack-dev-server eslint-plugin-cypress
 apply_patch global/create-react-app.patch
-apply_patch
 commit_all "Add Cypress with example component test"
 
 step
