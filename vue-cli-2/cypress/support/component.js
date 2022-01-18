efe1455588629cc58b232c028a07e2fcd722b2a7
Robert Guss <robertguss@pm.me>
2022-01-18 14:33:54 -0500

    added latest Cypress binary and added vuetify imports to component.js config

--- vue-cli-2/cypress/support/component.js
+++ vue-cli-2/cypress/support/component.js
@@ -1,4 +1,8 @@
 import "cypress-real-events/support";
+import Vue from "vue";
+import Vuetify from "vuetify";
+
+Vue.use(Vuetify);
 
 Cypress.Commands.add("vue", () => {
   return cy.wrap(Cypress.vueWrapper);
