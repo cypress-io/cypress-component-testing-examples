333471d443f6b1d8a70a3699b5e244a640040700
Robert Guss <robertguss@pm.me>
2022-01-19 14:09:11 -0500

    added test to confirm date picker selection works properly

--- create-react-app-ant-design/src/components/Datepicker.cy.js
+++ create-react-app-ant-design/src/components/Datepicker.cy.js
@@ -1,9 +1,28 @@
 import { mount } from "@cypress/react";
 import Datepicker from "./Datepicker";
+import dayjs from "dayjs";
 
-it("mounts", () => {
-  mount(<Datepicker />);
+const today = dayjs().format("YYYY-MM-DD");
+const dayOfMonth = dayjs().date();
 
-  cy.getBySel("date-picker").click();
-  cy.get(".ant-picker").should("have.class", "ant-picker-focused");
+describe("Datepicker", () => {
+  it("the datepicker opens after clicking in the input", () => {
+    mount(<Datepicker />);
+
+    cy.get(".ant-picker-dropdown").should("not.exist");
+    cy.getBySel("date-picker").click();
+    cy.get(".ant-picker-dropdown").should(
+      "not.have.class",
+      "ant-picker-dropdown-hidden"
+    );
+  });
+
+  it.only("after selecting a date with the datepicker, the input has the correct date and value", () => {
+    mount(<Datepicker />);
+
+    cy.getBySel("date-picker").invoke("val").should("equal", "");
+    cy.getBySel("date-picker").click();
+    cy.contains("td", dayOfMonth).click();
+    cy.getBySel("date-picker").invoke("val").should("equal", today);
+  });
 });
