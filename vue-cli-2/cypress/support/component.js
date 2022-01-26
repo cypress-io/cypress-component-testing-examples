6c36acb0a13e62b84489f7cd5b503113baae12c2
Ely Lucas <ely@meta-tek.net>
2022-01-26 13:32:32 -0700

    mount command fixes

--- vue-cli-2/cypress/support/component.js
+++ vue-cli-2/cypress/support/component.js
@@ -1,32 +1,41 @@
-import { mount } from '@cypress/vue'
-import 'vuetify/dist/vuetify.min.css'
-import "cypress-real-events/support";
-import Vuetify, { VApp } from 'vuetify/lib'
+import { mount } from '@cypress/vue';
+import 'vuetify/dist/vuetify.min.css';
+import 'cypress-real-events/support';
+import Vue from 'vue';
+import Vuetify, { VApp } from 'vuetify/lib';
 
 document.head.appendChild(Cypress.$(`<link href="https://cdn.jsdelivr.net/npm/@mdi/font@6.x/css/materialdesignicons.min.css" rel="stylesheet">`)[0])
 
 Cypress.Commands.add('mount', (component, options = {}) => {
-  const vuetify = new Vuetify({})
-  return mount({
-    render: (h) => {
-      return h(VApp, { vuetify }, [
-        h(component, {
-          vuetify,
-          props: options.propsData,
-          on: {
-            ...options.listeners,
-          }
-        })
-      ])
-    },
-  }, {
-    ...options,
-    plugins: [Vuetify],
-    vuetify,
-    listeners: options.listeners
-  })
-})
+  // Setup options object
+  options.extensions = options.extensions || {};
+  options.extensions.plugins = options.extensions.plugins || [];
+  options.extensions.components = options.extensions.components || {};
 
-Cypress.Commands.add("getBySel", (selector, ...args) => {
+  /* Add any global plugins */
+  Vue.use(Vuetify);
+  const vuetify = new Vuetify({});
+  options.vuetify = vuetify;
+
+  return mount(
+    {
+      render: (h) => {
+        return h(VApp, { vuetify }, [
+          h(component, {
+            vuetify,
+            props: options.propsData,
+            on: {
+              ...options.listeners,
+            },
+            extensions: options.extensions,
+          }),
+        ]);
+      },
+    },
+    options
+  );
+});
+
+Cypress.Commands.add('getBySel', (selector, ...args) => {
   return cy.get(`[data-test=${selector}]`, ...args);
 });
