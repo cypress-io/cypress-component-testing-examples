5d6a8d9829151422a4db9c88b3e19fead3198020
Craig Floyd <cfloyd@applied-insight.com>
2022-02-01 14:54:22 -0500

    vue2 mocked router example

--- vue-cli-2/src/components/RouterExample.vue
+++ vue-cli-2/src/components/RouterExample.vue
@@ -1,30 +1,33 @@
 <template>
   <div class="hello">
-    <h1>{{ myParam }}</h1>
+    
+    <h1>This component reads from and writes to a query param</h1>
+    <div>{{ myParam }}</div>
+
     <button @click="updateQueryParams">updateQueryParams</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: "RouterExample",
   props: {
     msg: String,
   },
   computed: {
+    // Current value of myParam
     myParam() {
       return this.$route.query.myParam;
     },
   },
   methods: {
-    // QUESTION: I want to prove that this function is called by stubbing it. I need to prevent the this.$router call, because it doesn't exist in a component test. 
-    //  Or I need to know the right way of injecting it in a component test
+    // Sets a new value
     updateQueryParams() {
       this.$router
         .push({
           name: this.$route.name,
           query: {
-            myParam: 'b'
+            myParam: 'newVal'
           },
         })
     },
@@ -34,18 +37,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
