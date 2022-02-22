118f8ba320f08e250bdf8f1162d764744f1e9e6b
Kevin Old <kevin@cypress.io>
2022-02-22 13:53:51 -0600

    update setup-create-react-app with latest v10 binary and code coverage beta

--- scripts/setup-create-react-app.sh
+++ scripts/setup-create-react-app.sh
@@ -18,13 +18,13 @@ cmd cd $dir
 commit_all "Run: ${commands[-2]}"
 
 step
-cmd yarn add -D cypress @cypress/react @cypress/webpack-dev-server eslint-plugin-cypress
+cmd yarn add -D https://cdn.cypress.io/beta/npm/10.0.0/linux-x64/circle-10.0-release-e7718f7489276cac2e8ad71bc57a627eb0135fbd/cypress.tgz @cypress/react @cypress/webpack-dev-server eslint-plugin-cypress
 apply_patch global/create-react-app.patch
 apply_patch
 commit_all "Add Cypress with example component test"
 
 step
-cmd yarn add -D @cypress/code-coverage @cypress/instrument-cra
+cmd yarn add -D @cypress/code-coverage@3.10.0-dev.1 @cypress/instrument-cra
 apply_patch global/create-react-app-code-coverage.patch
 commit_all "Configure Cypress Code Coverage plugin"
 
