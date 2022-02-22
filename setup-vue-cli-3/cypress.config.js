7dd07d03642ae687c27604084e977f0c5cc18ce6
Kevin Old <kevin@cypress.io>
2022-02-22 11:16:50 -0600

    Configure Cypress Code Coverage plugin

--- setup-vue-cli-3/cypress.config.js
+++ setup-vue-cli-3/cypress.config.js
@@ -1,11 +1,20 @@
 const { defineConfig } = require('cypress')
 const { devServer } = require('@cypress/webpack-dev-server')
 const webpackConfig = require('@vue/cli-service/webpack.config')
+const codeCoverageTask = require("@cypress/code-coverage/task")
 
 module.exports = defineConfig({
+  coverage: true,
+  codeCoverage: {
+    exclude: "cypress/**/*.*",
+  },
   // Component testing, JavaScript, Vue CLI, Webpack
   component: {
     devServer,
-    devServerConfig: { webpackConfig }
+    devServerConfig: { webpackConfig },
+    setupNodeEvents(on, config) {
+      codeCoverageTask(on, config);
+      return config;
+    },
   },
 })
