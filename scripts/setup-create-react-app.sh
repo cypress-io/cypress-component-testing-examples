3f2ba5da2fbeac83ce0df8a2db8f902e3b5e279c
Kevin Old <kevin@cypress.io>
2022-02-23 15:00:53 -0600

    finalize script and patches for setup-create-react-app

--- scripts/setup-create-react-app.sh
+++ scripts/setup-create-react-app.sh
@@ -23,9 +23,9 @@ apply_patch global/setup-create-react-app.patch
 apply_patch
 commit_all "Add Cypress with example component test"
 
-# step
-# cmd yarn add -D @cypress/code-coverage@3.10.0-dev.1 @cypress/instrument-cra
-# apply_patch global/setup-create-react-app-code-coverage.patch
-# commit_all "Configure Cypress Code Coverage plugin"
+step
+cmd yarn add -D @cypress/code-coverage@3.10.0-dev.1 @cypress/instrument-cra
+apply_patch global/setup-create-react-app-code-coverage.patch
+commit_all "Configure Cypress Code Coverage plugin"
 
 finalize
