fca8a6aaebf92629554eab1c6a75f95d28c385da
Kevin Old <kevin@cypress.io>
2022-02-25 15:00:30 -0600

    Add Cypress with example component test

---
+++ setup-create-react-app-tailwind/cypress/support/component.js
@@ -0,0 +1,8 @@
+// Import commands.js using ES2015 syntax:
+import './commands'
+
+// Ensure tailwind styles are loaded:
+import 'tailwindcss/dist/tailwind.css'
+
+// Ensure global app styles are loaded:
+import '../../src/index.css'
