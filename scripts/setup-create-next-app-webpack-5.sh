3f9e58732ca85855d9e4cd2c50b41da84a3085b2
Kevin Old <kevin@cypress.io>
2022-03-21 17:16:12 -0500

    fix up setup create next app install scripts

--- scripts/setup-create-next-app-webpack-5.sh
+++ scripts/setup-create-next-app-webpack-5.sh
@@ -18,7 +18,7 @@ cmd cd $dir
 commit_all "Run: ${commands[-2]}"
 
 step
-cmd yarn add -D https://cdn.cypress.io/beta/npm/10.0.0/linux-x64/10.0-release-188b9a742ee2ef51102167bfd84b3696a3f72a26/cypress.tgz webpack @cypress/react @cypress/webpack-dev-server
+cmd yarn add -D https://cdn.cypress.io/beta/npm/10.0.0/linux-x64/10.0-release-188b9a742ee2ef51102167bfd84b3696a3f72a26/cypress.tgz webpack webpack-dev-server @cypress/react @cypress/webpack-dev-server html-webpack-plugin
 apply_patch global/setup-create-next-app.patch
 apply_patch 
 commit_all "Add Cypress with example component and page tests"
